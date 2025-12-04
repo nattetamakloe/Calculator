8cc064311fb314c4550935cc06acba08cce9a08b
N8DGR8 <38409384+nattetamakloe@users.noreply.github.com>
2025-12-04 16:46:55 +0100

    Create operator functions

---
+++ script.js
@@ -0,0 +1,40 @@
+/* 2. Variabler för en operation
+Skapa tre variabler: */
+let num1;
+let operator;
+let num2;
+
+/* 1. Grundläggande funktioner */
+function add(num1, num2) {
+    return num1 + num2;
+};
+
+function subtract(num1, num2) {
+    return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+
+function divide(num1, num2) {
+    return num1 / num2;
+};
+
+/* 3. Skapa funktionen operate som:
+tar en operator och två nummer
+anropar motsvarande matematiska funktion */
+function operate(operate, num1, num2) {
+    switch(operate) {
+        case "+": return add(num1, num2);
+            
+        case "-": return subtract(num1, num2);
+        
+        case "*": return multiply(num1, num2);
+    
+        case "/": return divide(num1, num2);
+
+        default: return "Invalid operator"
+    }
+}
+
