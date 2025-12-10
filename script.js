7ac72d85139c327b5abd6979bc781bfce6a42f92
N8DGR8 <38409384+nattetamakloe@users.noreply.github.com>
2025-12-10 11:18:12 +0100

    Restructure and add conditions to chooseOperator

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-/* 2. Variabler för en operation
-Skapa tre variabler: */
 let num1;
 let operator;
 let num2;
+
+const display = document.querySelector('#display');
 
 const buttons = document.querySelectorAll(".button").forEach(btn => {
     btn.addEventListener('click', () => {
@@ -12,47 +12,7 @@ const buttons = document.querySelectorAll(".button").forEach(btn => {
 
         updateDisplay(value, op, action);
     })
-
 });
-
-const display = document.querySelector('#display');
-
-
-/* 1. Grundläggande funktioner */
-function add(num1, num2) {
-    return num1 + num2;
-};
-
-function subtract(num1, num2) {
-    return num1 - num2;
-};
-
-function multiply(num1, num2) {
-    return num1 * num2;
-};
-
-function divide(num1, num2) {
-    return num1 / num2;
-};
-
-/* 3. Skapa funktionen operate som:
-tar en operator och två nummer
-anropar motsvarande matematiska funktion */
-function operate(operator, num1, num2) {
-    switch (operator) {
-        case "add": return add(num1, num2);
-
-        case "subtract": return subtract(num1, num2);
-
-        case "multiply": return multiply(num1, num2);
-
-        case "divide": return divide(num1, num2);
-
-        default: return "Invalid operator"
-    }
-}
-
-
 
 function updateDisplay(value, op, action) {
 
@@ -79,9 +39,15 @@ function appendNumber(value) {
 };
 
 function chooseOperator(op) {
-    num1 = Number(display.textContent);
-    operator = op;
-    display.textContent = 0;
+    if (operator !== undefined && display.textContent !== '0') {
+        compute();
+        operator = op;
+    } else {
+        num1 = Number(display.textContent);
+        display.textContent = 0;
+        operator = op;
+    }
+
 
 }
 
@@ -93,5 +59,34 @@ function compute() {
     operator = undefined;
 };
 
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "add": return add(num1, num2);
+
+        case "subtract": return subtract(num1, num2);
+
+        case "multiply": return multiply(num1, num2);
+
+        case "divide": return divide(num1, num2);
+
+        default: return "Invalid operator"
+    }
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+};
+
+function subtract(num1, num2) {
+    return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+
+function divide(num1, num2) {
+    return num1 / num2;
+};
 
 
